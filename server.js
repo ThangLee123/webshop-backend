972e592698b1218c2499a342cbec60e10db5ecfc
Thang <thangle220909@gmail.com>
2022-12-25 16:27:03 +0700

    change port

--- server.js
+++ server.js
@@ -56,5 +56,6 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Serve at http://localhost:5000");
+  // console.log("Serve at http://localhost:5000");
+  console.log("Serve at port ", port);
 });
